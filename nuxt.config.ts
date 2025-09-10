46ba8d057f3eebd2df6c1838b16d6718f8e8227e
Zoey Kaiser <zoeykaiser8@gmail.com>
2025-09-10 13:37:15 +0200

    Installed vercel AI packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui'
+  ],
+  runtimeConfig: {
+    openaiApiKey: '',
+  }
 })
